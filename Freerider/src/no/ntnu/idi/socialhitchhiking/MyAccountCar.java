4097400e180c766173cf86ff674d44a1935e8b82
Made Ziius <made.ziius@gmail.com>
2013-03-04 15:47:41 +0000

    Camera in my account my car

--- Freerider/src/no/ntnu/idi/socialhitchhiking/MyAccountCar.java
+++ Freerider/src/no/ntnu/idi/socialhitchhiking/MyAccountCar.java
@@ -1,16 +1,40 @@
 package no.ntnu.idi.socialhitchhiking;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MyAccountCar extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.my_account_car);
+    private static final int CAMERA_REQUEST = 1337; 
+    private ImageView imageView;
+    
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.my_account_car);
+	        this.imageView = (ImageView)this.findViewById(R.id.cameraView);
+	        Button photoButton = (Button) this.findViewById(R.id.cameraButton);
+	        photoButton.setOnClickListener(new View.OnClickListener() {
+
+	            @Override
+	            public void onClick(View v) {
+	                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); 
+	                startActivityForResult(cameraIntent, 1337); 
+	  
+	            }
+	        });
+	    }
+
+	    protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
+	        if (requestCode == 1337 && resultCode == RESULT_OK) {  
+	            Bitmap photo = (Bitmap) data.getExtras().get("data"); 
+	            imageView.setImageBitmap(photo);
+	        }  
+	    } 
 	}
 
-
-}
--- Freerider/src/no/ntnu/idi/socialhitchhiking/My_account.java
+++ Freerider/src/no/ntnu/idi/socialhitchhiking/My_account.java
@@ -1,7 +1,7 @@
 package no.ntnu.idi.socialhitchhiking;
-import android.os.Bundle;
 import android.app.TabActivity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
