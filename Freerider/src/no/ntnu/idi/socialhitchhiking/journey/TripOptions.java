d546ccd54308d2063105a0e1f92a844d6efb4c04
jltrigo <jltrigo@gmail.com>
2013-03-06 15:21:12 +0100

    Changes in trip options. Pressing seats now displays a custom NumberPicker but it has to be improved so when plus or minus buttons are pressed, the text displayed changes.

--- Freerider/src/no/ntnu/idi/socialhitchhiking/journey/TripOptions.java
+++ Freerider/src/no/ntnu/idi/socialhitchhiking/journey/TripOptions.java
@@ -43,12 +43,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.DialogFragment;
+import android.text.style.BulletSpan;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -61,6 +66,8 @@ public class TripOptions extends SocialHitchhikingActivity {
 	private NumberPicker seats;
 	private Journey currentJourney;
 	private TripPreferences tripPreferences;
+	private Integer selectedPrivacy;
+	private Visibility privacyPreference;
     private PropertyChangeListener propLis = new PropertyChangeListener() {
 		@Override
 		public void propertyChange(PropertyChangeEvent event) {
@@ -168,36 +175,82 @@ public class TripOptions extends SocialHitchhikingActivity {
 		b.show();
     }
     void setSeats(){
+//    	counter = 0;
+//    	Button add = (Button) findViewById(R.id.bAdd);
+//    	Button sub = (Button) findViewById(R.id.bSub);
+//    	final EditText display = (EditText) findViewById(R.id.numberofseats);
+    	
+    	
     	//TODO Update seats in TripPreferences and show it on TripOptions Activity
-	    AlertDialog.Builder b = new AlertDialog.Builder(this);
-		b.setTitle("Seats Avalilable");
-		b.setMessage("Set Seats available");
-		b.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				//TODO number picker for selecting seats. Store data in TripPreferences atributte in Journey/Ride
-				//seats = new NumberPicker(getApp().getApplicationContext());
-				
-			}
-		});
-		b.setNegativeButton("Cancel", null);
-		b.show();
+    	    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    	    builder.setTitle("Seats");	
+    	    builder.setMessage("Select seats available");
+    	    // Get the layout inflater
+    	    LayoutInflater inflater = this.getLayoutInflater();
+    	    
+//    	    add.setOnClickListener(new View.OnClickListener() {
+//
+//    	    	public void onClick(View v) {
+//    	    		counter++;
+//    	    		display.setText( "" + counter);
+//    	    	}
+//    	    });
+//    	    sub.setOnClickListener(new View.OnClickListener() {
+//
+//    	    	public void onClick(View v) {
+//    	    		counter--;
+//    	    		display.setText( "" + counter);
+//    	    	}
+//    	    });
+    	    // Inflate and set the layout for the dialog
+    	    // Pass null as the parent view because its going in the dialog layout
+    	    
+    	    builder.setView(inflater.inflate(R.layout.number_picker, null));
+    	    
+    	    // Add action buttons
+    	    builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int id) {
+				       // sign in the user ...
+				}
+    	    });
+    	    builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+    	    	public void onClick(DialogInterface dialog, int id) {
+    	    		//
+    	    	}
+    	    });      
+    	    builder.show();
     }
     void setPrivacy(){
     	//TODO Change this to save visibility preferences in current Journey/trip.
+    	selectedPrivacy=-1;
     	AlertDialog.Builder builder = new AlertDialog.Builder(this);
     	builder.setTitle("Set Privacy");
     	builder.setSingleChoiceItems(R.array.privacy_setting, -1, new DialogInterface.OnClickListener() {
     	    public void onClick(DialogInterface dialog, int item) {
     	        //Toast.makeText(getApplicationContext(), items[item], Toast.LENGTH_SHORT).show();
-    	        //int selected = item;
-    	        
+    	        selectedPrivacy = item;
     	    }
     	});
     	builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 //currentJourney.setVisibility(Visibility.FRIENDS_OF_FRIENDS);
+            	switch (selectedPrivacy) {
+				case 0:
+					privacyPreference=Visibility.FRIENDS;
+					break;
+				case 1:
+					privacyPreference=Visibility.FRIENDS_OF_FRIENDS;
+					break;
+				case 2:
+					privacyPreference=Visibility.PUBLIC;
+					break;
+				default:
+					break;
+				}
+            	Toast.makeText(getApplicationContext(), privacyPreference.toString(), Toast.LENGTH_SHORT).show();
             }
+          
         });
         builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
@@ -210,23 +263,26 @@ public class TripOptions extends SocialHitchhikingActivity {
     void setExtras(){
     	//TODO This should record which are the Trip extras and write it in Journey/Ride in the attribute TripPreferences
     	final CharSequence[] items = {"Breaks", "Animals", "Music", "Talking", "Smoking"};
-    	final boolean [] selected = {false, false, false, false, false};
+    	final boolean [] selectedExtras = {false, false, false, false, false};
 
     	AlertDialog.Builder builder = new AlertDialog.Builder(this);
     	builder.setTitle("Pick extras");
-    	builder.setMultiChoiceItems(items, null, new DialogInterface.OnMultiChoiceClickListener() {
+    	builder.setMultiChoiceItems(R.array.preferences_array, null, new DialogInterface.OnMultiChoiceClickListener() {
 			
 			@Override
 			public void onClick(DialogInterface dialog, int which, boolean isChecked) {
 				// TODO Auto-generated method stub
 				//Toast.makeText(getApplicationContext(), items[which], Toast.LENGTH_SHORT).show();
-				//selected[which] = isChecked;
+				selectedExtras[which] = isChecked;
 			}
     	});
     	builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-            
                //TODO Check which check boxes are marked and update the tripPreferences
+//            	for(int i=0;i<items.length;i++){
+//            		if
+//            	}
+//            	Toast.makeText(getApplicationContext(), items[which], Toast.LENGTH_SHORT).show();
             }
         });
     	 builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -243,5 +299,4 @@ public class TripOptions extends SocialHitchhikingActivity {
 		Intent intent = new Intent(TripOptions.this, Main.class);
 		startActivity(intent);
 	}
-
 }
