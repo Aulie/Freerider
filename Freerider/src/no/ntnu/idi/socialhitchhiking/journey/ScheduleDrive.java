cb66c42efaaaa2f366ac0fec70055de84880bf93
jltrigo <Jose@172.16.254.128>
2013-04-19 13:29:00 +0200

    Close reuse old on empty fixed

--- Freerider/src/no/ntnu/idi/socialhitchhiking/journey/ScheduleDrive.java
+++ Freerider/src/no/ntnu/idi/socialhitchhiking/journey/ScheduleDrive.java
@@ -40,6 +40,7 @@ import no.ntnu.idi.freerider.protocol.RouteRequest;
 import no.ntnu.idi.freerider.protocol.RouteResponse;
 import no.ntnu.idi.freerider.protocol.UserRequest;
 import no.ntnu.idi.freerider.protocol.UserResponse;
+import no.ntnu.idi.socialhitchhiking.Main;
 import no.ntnu.idi.socialhitchhiking.R;
 import no.ntnu.idi.socialhitchhiking.client.RequestTask;
 import no.ntnu.idi.socialhitchhiking.map.MapRoute;
@@ -211,6 +212,8 @@ public class ScheduleDrive extends SocialHitchhikingActivity {
 			listRoute.setAdapter(routeAdap);
 		}
 		if(routeAdap.getCount()==0){
+			Intent intent = new Intent(ScheduleDrive.this, Main.class);
+			startActivity(intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
 			Toast.makeText(this, "You have no saved routes", Toast.LENGTH_LONG).show();
 		}
 	}
