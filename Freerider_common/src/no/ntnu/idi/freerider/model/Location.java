4c81427b1e8e8cdf600dc948208cd2b9de92fe0f
Thomas Gjerde <thomasgjerde90@gmail.com>
2013-04-29 14:15:34 +0200

    Clean up

--- Freerider_common/src/no/ntnu/idi/freerider/model/Location.java
+++ Freerider_common/src/no/ntnu/idi/freerider/model/Location.java
@@ -17,27 +17,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-/**
- * @contributor(s): Freerider Team (Group 4, IT2901 Fall 2012, NTNU)
- * @version: 		1.0
- *
- * Copyright (C) 2012 Freerider Team.
- *
- * Licensed under the Apache License, Version 2.0.
- * You may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied.
- *
- * See the License for the specific language governing permissions
- * and limitations under the License.
- *
- */
+
 package no.ntnu.idi.freerider.model;
 
 import java.io.Serializable;
